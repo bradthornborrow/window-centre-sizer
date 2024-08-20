3d53033e8f1bde4b64e6c79d468dca2f77415596
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-08-20 00:02:46 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -27,8 +27,8 @@ const MESSAGE_FADE_TIME = 2000;
 
 export default class WindowCentreSizerExtension extends Extension {
     SIZES = [
-        [0.70, 0.76],
-        [0.80, 0.86],
+        [0.72, 0.76],
+        [0.82, 0.86],
     ];
 
     _flashMessage(message) {
